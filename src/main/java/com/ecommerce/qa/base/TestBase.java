c98f2e11e0665d6cdcf86607a6edeb61915a7cac
MadhaviBandara <madi.bandara@gmail.com>
2021-11-09 18:52:35 +0530

    Q4

--- src/main/java/com/ecommerce/qa/base/TestBase.java
+++ src/main/java/com/ecommerce/qa/base/TestBase.java
@@ -4,6 +4,7 @@ import com.ecommerce.qa.util.TestUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,6 +16,8 @@ public class TestBase {
 
     public static WebDriver driver;
     public static Properties prop;
+    public  static EventFiringWebDriver e_driver;
+    public static WebEventListener eventListener;
 
     public TestBase(){
         try{
@@ -41,6 +44,11 @@ public class TestBase {
             System.setProperty("webdriver.gecko.driver","libs/geckodriver");
             driver = new FirefoxDriver();
         }
+        e_driver = new EventFiringWebDriver(driver);
+        // Now create object of EventListerHandler to register it with EventFiringWebDriver
+        eventListener = new WebEventListener();
+        e_driver.register(eventListener);
+        driver = e_driver;
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
--- target/cucumber-report/report.js
+++ target/cucumber-report/report.js
@@ -89,23 +89,22 @@ formatter.match({
   "location": "AddItemsToCartSteps.user_logged_in()"
 });
 formatter.result({
-  "duration": 36956311900,
+  "duration": 29284585700,
   "status": "passed"
 });
 formatter.match({
   "location": "AddItemsToCartSteps.page_validated()"
 });
 formatter.result({
-  "duration": 14323258600,
-  "status": "passed"
+  "duration": 10310214200,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//*[@id\u003d\u0027block_top_menu\u0027]/ul/li[1]/a\"}\n  (Session info: chrome\u003d94.0.4606.81)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:25:53\u0027\nSystem info: host: \u0027MADHI\u0027, ip: \u0027192.168.56.1\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_111\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 94.0.4606.81, chrome: {chromedriverVersion: 94.0.4606.61 (418b78f5838ed..., userDataDir: C:\\Users\\Dell\\AppData\\Local...}, goog:chromeOptions: {debuggerAddress: localhost:57308}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: d108788b3cb4cf58559d64274377bb76\n*** Element info: {Using\u003dxpath, value\u003d//*[@id\u003d\u0027block_top_menu\u0027]/ul/li[1]/a}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy15.click(Unknown Source)\r\n\tat com.ecommerce.qa.pages.ProfilePage.GoToWomensCategory(ProfilePage.java:38)\r\n\tat StepDefinitions.AddItemsToCartSteps.page_validated(AddItemsToCartSteps.java:30)\r\n\tat ✽.Then user navigates to item page(AddItemsToCart.feature:6)\r\n",
+  "status": "failed"
 });
 formatter.match({
   "location": "AddItemsToCartSteps.all_items_are_selected()"
 });
 formatter.result({
-  "duration": 6328150300,
-  "error_message": "org.openqa.selenium.TimeoutException: Expected condition failed: waiting for element to be clickable: [[ChromeDriver: chrome on WINDOWS (2543766985fcf210a48bd24a43e8d4f5)] -\u003e xpath: //*[@id\u003d\u0027layer_cart\u0027]/div[1]/div[2]/div[4]/span] (tried for 5 second(s) with 500 milliseconds interval)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027RAVEESHAPC\u0027, ip: \u0027192.168.1.3\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_302\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 95.0.4638.69, chrome: {chromedriverVersion: 94.0.4606.61 (418b78f5838ed..., userDataDir: C:\\Users\\ravee\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:65333}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 2543766985fcf210a48bd24a43e8d4f5\r\n\tat org.openqa.selenium.support.ui.WebDriverWait.timeoutException(WebDriverWait.java:95)\r\n\tat org.openqa.selenium.support.ui.FluentWait.until(FluentWait.java:272)\r\n\tat com.ecommerce.qa.pages.WomenCategoryPage.addItemToCart(WomenCategoryPage.java:75)\r\n\tat com.ecommerce.qa.pages.WomenCategoryPage.selectItems(WomenCategoryPage.java:79)\r\n\tat StepDefinitions.AddItemsToCartSteps.all_items_are_selected(AddItemsToCartSteps.java:40)\r\n\tat ✽.And user selects all items and adds to cart(AddItemsToCart.feature:7)\r\n",
-  "status": "failed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "AddItemsToCartSteps.obtain_invoice()"
@@ -212,33 +211,35 @@ formatter.match({
   "location": "Login.navigated_to_the_login_page()"
 });
 formatter.result({
-  "duration": 11016516800,
+  "duration": 16301661000,
   "status": "passed"
 });
 formatter.match({
   "location": "Login.login_page_title_validated()"
 });
 formatter.result({
-  "duration": 99558800,
-  "error_message": "java.lang.AssertionError: expected [Login - My Store] but found [508 Resource Limit Is Reached]\r\n\tat org.testng.Assert.fail(Assert.java:99)\r\n\tat org.testng.Assert.failNotEquals(Assert.java:1037)\r\n\tat org.testng.Assert.assertEqualsImpl(Assert.java:140)\r\n\tat org.testng.Assert.assertEquals(Assert.java:122)\r\n\tat org.testng.Assert.assertEquals(Assert.java:629)\r\n\tat org.testng.Assert.assertEquals(Assert.java:639)\r\n\tat StepDefinitions.Login.login_page_title_validated(Login.java:26)\r\n\tat ✽.And login page title validated(Login.feature:5)\r\n",
-  "status": "failed"
+  "duration": 100038300,
+  "status": "passed"
 });
 formatter.match({
   "location": "Login.login_image_validated()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 292955600,
+  "status": "passed"
 });
 formatter.match({
   "location": "Login.login_credentials_provided()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 10230819300,
+  "status": "passed"
 });
 formatter.match({
   "location": "Login.navigate_to_profile()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 14350000,
+  "status": "passed"
 });
 });
